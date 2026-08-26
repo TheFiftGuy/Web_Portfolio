67f42005c2afa96a4ee2958b0b2613f1760d2b02
DeclanMcG <mcguinness.declan0@gmail.com>
2026-08-25 20:36:35 -0400

    Added full contact Me email functionality.

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+function sendEmail()    {
+    const templateParams = {
+        name: document.querySelector('#contact-name').value,
+        email: document.querySelector('#contact-email').value,
+        subject: document.querySelector('#contact-subject').value,
+        message: document.querySelector('#contact-message').value,
+        phoneNumber: document.querySelector('#contact-phone').value,
+        time: Date(),
+    };
+    if( templateParams.name != "" && templateParams.email != "" && templateParams.subject != "" && templateParams.message != "")    {
+        emailjs.send('service_97h76w9', 'template_ok8l80p', templateParams).then(
+            (response) => {
+                alert("Email sent successfully!");
+            },
+            (error) => {
+                alert("Email failed to send.");
+            },
+        );
+    }
+    else{
+        alert("Failed to send email. Please make sure to fill in all fields marked with *");
+    }
+}
